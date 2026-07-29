045b6788bd2212188b218ed8a9d0a2eb3733f010
samyaksharma0007 <169792168+samyaksharma0007@users.noreply.github.com>
2026-07-29 22:44:06 +0530

    Added Relational Operator

---
+++ Operator/Relational_Operator.cpp
@@ -0,0 +1,25 @@
+//Relational Operators
+// >
+// <
+// >=
+// <=
+// ==
+// !=
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int b = 3;
+    
+    cout << (10>3) << endl; // greater than
+    cout << (10<3) << endl; // less than
+    cout << (10>=3) << endl; // greater than equal to
+    cout << (10<=3) << endl; // less than equal to
+    cout << (10==3) << endl; // equal to
+    cout << (10!=3) << endl; // not equal to
+
+    return 0;
+}
