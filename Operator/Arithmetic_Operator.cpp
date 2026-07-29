5609f8f157a86bc5291e105d7900e5688dc8fd13
samyaksharma0007 <169792168+samyaksharma0007@users.noreply.github.com>
2026-07-29 22:35:24 +0530

    Added Arithmetic Operator

---
+++ Operator/Arithmetic_Operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int b = 5;
+    
+    //Arithmetic Operators
+    cout << (a+b) << endl; //addition
+    cout << (a-b) << endl; //subtraction
+    cout << (a*b) << endl; //multiplication
+    cout << (a/b) << endl; //divison
+    cout << (a%b) << endl; //remainder
+
+    return 0;
+}
