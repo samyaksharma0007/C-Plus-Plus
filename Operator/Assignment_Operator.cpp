d918733b7629688050a3ffce3cebd91d840ea790
samyaksharma0007 <169792168+samyaksharma0007@users.noreply.github.com>
2026-07-30 12:12:39 +0530

    Added Assignment Operator

---
+++ Operator/Assignment_Operator.cpp
@@ -0,0 +1,34 @@
+//Assignment Operators
+// = - equal to
+// += - equal then plus with variable
+// -= - equal then subtraction with variable
+// *= - equal then multiplication with variable
+// /= - equal then divison with variable
+// %= - equal then modulus with variable
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int arr = 5;
+    cout << arr << endl;
+
+    arr += 10;
+    cout << arr << endl;
+
+    arr -= 8;
+    cout << arr << endl;
+
+    arr *= 2;
+    cout << arr << endl;
+
+    arr /= 3;
+    cout << arr << endl;
+
+    arr %= 5;
+    cout << arr << endl;
+
+
+    return 0;
+}
